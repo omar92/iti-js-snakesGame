0e4a86791b6db20dcd317c0cf97568652d645799
ADMINRG-SSONAJL\Kai Sama <0ultrakai0@gmail.com>
2017-12-01 14:35:06 +0200

    blocks gets destroyed when its value reaches zero

--- js/level.js
+++ js/level.js
@@ -1,7 +1,7 @@
 var Block = function(pos, val, sprite)
 {
     this.position = pos;
-    this.value = val;
+    this.text = val;
     this.sprite = sprite;
 }
 
@@ -28,14 +28,17 @@ var Map = function(width, blockSize, player, end)
 
     var CheckCollision = function(block, player)
     {
-        block.value--;
+        let val = parseInt(this.collidedBlock.text.text);
+        val--;
 
-        if (block.value <= 0)
+        if (val <= 0)
         {
-            this.DestroyBlock(block);
+            this.DestroyBlock(this.collidedBlock);
         }
 
-        //console.log("colliding");
+        this.collidedBlock.text.text = val.toString();
+
+        console.log(val);
     }
 
     var IsOutOfMap = function(block, end)
@@ -50,9 +53,13 @@ var Map = function(width, blockSize, player, end)
         for (let index = 0; index < blocksNum; index++) 
         {
             let sprite = game.add.sprite(index * blockSize, -100, "empty");
+            var style = { font: "32px Arial", fill: "#ff0044", wordWrap: true, wordWrapWidth: sprite.width, align: "center", backgroundColor: "#ffff00" };
+            text = game.add.text(0, 0, "10", style);
+
+            text.anchor.set(0.5);
             game.physics.arcade.enable(sprite); 
             //sprite.body.gravity.y = 50;
-            let block = new Block({x:index * blockSize, y:-100}, 10, sprite);
+            let block = new Block({x:index * blockSize, y:-100}, text, sprite);
             this.blocks.push(block);
         }
 
@@ -64,7 +71,19 @@ var Map = function(width, blockSize, player, end)
 
     this.DestroyBlock = function(block)
     {
+        block.sprite.destroy(true);
+        block.text.destroy(true);
+        block = undefined;
 
+        for (var index = 0; index < this.blocks.length; index++) 
+        {
+            if (this.blocks[index] == undefined)
+            {
+                break;
+            }         
+        }
+
+        this.blocks.splice(index, 1);
     }
 
     this.DestroyBlocks = function()
@@ -88,10 +107,13 @@ var Map = function(width, blockSize, player, end)
         
         for (var index = 0; index < this.blocks.length; index++) 
         {
-            game.physics.arcade.overlap(player.head, this.blocks[index].sprite, CheckCollision, null, this);            
+            this.collidedBlock = this.blocks[index];
+            this.blocks[index].text.x = Math.floor(this.blocks[index].sprite.x + this.blocks[index].sprite.width / 2);
+            this.blocks[index].text.y = Math.floor(this.blocks[index].sprite.y + this.blocks[index].sprite.height / 2);
+            game.physics.arcade.overlap(this.blocks[index].sprite, player.content, CheckCollision, null, this);            
             game.physics.arcade.overlap(end, this.blocks[index].sprite, IsOutOfMap, null, this);            
         }
 
-        this.MoveBlocks();        
+        this.collidedBlock = undefined;     
     }
 }
